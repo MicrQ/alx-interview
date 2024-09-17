85ab08dc05f778e81afffcb3b04f3203bf2da9a1
AbenetGebre <mail.micrq@proton.me>
2024-09-17 17:23:32 +0300

    modified

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,51 +2,34 @@
 """ prime game """
 
 
-def sieve_of_eratosthenes(max_n):
-    """ helper """
-    primes = [True] * (max_n + 1)
-    primes[0] = primes[1] = False
-    p = 2
-    while (p * p <= max_n):
-        if primes[p]:
-            for i in range(p * p, max_n + 1, p):
-                primes[i] = False
-        p += 1
-    prime_list = []
-    for p in range(2, max_n + 1):
-        if primes[p]:
-            prime_list.append(p)
-    return prime_list
-
-
 def isWinner(x, nums):
     """ prime game """
-    max_n = max(nums)
-    primes = sieve_of_eratosthenes(max_n)
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        primes_used = [False] * (n + 1)
-        prime_count = 0
-
-        for prime in primes:
-            if prime > n:
-                break
-            if not primes_used[prime]:
-                prime_count += 1
-                for multiple in range(prime, n + 1, prime):
-                    primes_used[multiple] = True
-
-        if prime_count % 2 == 1:
-            maria_wins += 1
-        else:
-            ben_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    if x <= 0 or nums is None:
         return None
+    if x != len(nums):
+        return None
+    ben = 0
+    maria = 0
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        mul(a, i)
+    for i in nums:
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+    if ben > maria:
+        return "Ben"
+    if maria > ben:
+        return "Maria"
+    return None
+
+
+def mul(l, x):
+    """ helper function """
+    for i in range(2, len(l)):
+        try:
+            l[i * x] = 0
+        except (ValueError, IndexError):
+            break
